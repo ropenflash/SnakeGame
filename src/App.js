f2caea414223eaca5ee8d1f4594006fcc3488455
Rishab Gupta <gupta.rishab17@gmail.com>
2019-09-09 21:10:40 +0530

    added food component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Snake from "./Snake";
+import Food from "./Food";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      food: [6, 8],
       snakeDots: [[0, 0], [2, 0]]
     };
   }
   render() {
-    const { snakeDots } = this.state;
+    const { snakeDots, food } = this.state;
     return (
       <div className="game-area">
         <Snake snakeDots={snakeDots} />
+        <Food dot={food} />
       </div>
     );
   }
---
+++ src/Food.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default ({ dot }) => {
+  const style = {
+    left: `${dot[0]}%`,
+    top: `${dot[1]}%`
+  };
+  return <div className="snake-food" style={style}></div>;
+};
