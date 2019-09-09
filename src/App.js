952b324c34d045875d4909d26d346994b3809f79
Rishab Gupta <gupta.rishab17@gmail.com>
2019-09-09 21:04:12 +0530

    Added Snake component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Snake from "./Snake";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      snakeDots: [[0, 0], [2, 0]]
+    };
+  }
+  render() {
+    const { snakeDots } = this.state;
+    return (
+      <div className="game-area">
+        <Snake snakeDots={snakeDots} />
+      </div>
+    );
+  }
 }
-
 export default App;
